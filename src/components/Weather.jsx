89a7cc529ad2878e4a8e6ff71248ed83807bf742
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-06-09 19:49:02 +0100

    Added styling for weather component

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,17 +1,26 @@
-import { Image, Text, VStack } from "@chakra-ui/react";
+import { Flex, HStack, Image, Text, VStack } from "@chakra-ui/react";
 
 const Weather = ({ weatherData }) => {
   return (
-    <div>
+    <Flex justify="center">
       <VStack>
-        <Text>{weatherData.name}</Text>
-        <Text>{Math.round(weatherData.main.temp)}°C</Text>
-        <Text>{weatherData.weather[0].main}</Text>
-        <Image
-          src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
-        ></Image>
+        <Text fontSize="7xl" fontWeight="bold">
+          {weatherData.name}
+        </Text>
+        <HStack>
+          <VStack p="5" spacing="10">
+            <Text fontSize="5xl">{Math.round(weatherData.main.temp)}°C</Text>
+            <Text>Feels like {Math.round(weatherData.main.feels_like)}°C</Text>
+          </VStack>
+          <VStack p="5" spacing="10">
+            <Text>{weatherData.weather[0].main}</Text>
+            <Image
+              src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+            ></Image>
+          </VStack>
+        </HStack>
       </VStack>
-    </div>
+    </Flex>
   );
 };
 
